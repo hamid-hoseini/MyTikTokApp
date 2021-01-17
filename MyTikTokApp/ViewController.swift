078200670042938d92afcc7e10035f9fdb711f12
Hamid <mail.hamid.hoseini@gmail.com>
2021-01-16 20:01:14 -0800

    Initial Commit

---
+++ MyTikTokApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTikTokApp
+//
+//  Created by Hamid Hoseini on 1/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
