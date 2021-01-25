21266194ff1ca2e3c50dfac681b50feb842f41aa
Hamid <mail.hamid.hoseini@gmail.com>
2021-01-24 18:46:55 -0800

    For you & following buttons

--- MyTikTokApp/Controllers/Core/HomeViewController.swift
+++ MyTikTokApp/Controllers/Core/HomeViewController.swift
@@ -12,10 +12,19 @@ class HomeViewController: UIViewController {
     let horizontalScrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.bounces = false
-        scrollView.backgroundColor = .red
+        //scrollView.backgroundColor = .red
         scrollView.isPagingEnabled = true
         scrollView.showsHorizontalScrollIndicator = false
         return scrollView
+    }()
+    
+    let control: UISegmentedControl = {
+        let titles = ["Following",  "For You"]
+        let control = UISegmentedControl(items: titles)
+        control.backgroundColor = nil
+        control.selectedSegmentTintColor = .white
+        control.selectedSegmentIndex = 1
+        return control
     }()
     
     let forYouPageViewController = UIPageViewController(
@@ -40,12 +49,25 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .red
         view.addSubview(horizontalScrollView)
         setupFeed()
+        horizontalScrollView.delegate = self
         horizontalScrollView.contentOffset = CGPoint(x: view.width, y: 0)
+        setupHeaderButtons()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         horizontalScrollView.frame = view.bounds
+    }
+    
+    func setupHeaderButtons() {
+        control.addTarget(self, action: #selector(didChangeSegmentControl(_:)), for: .valueChanged)
+        navigationItem.titleView = control
+    }
+    
+    @objc private func didChangeSegmentControl(_ sender: UISegmentedControl) {
+        horizontalScrollView.setContentOffset(CGPoint(x: view.width * CGFloat(sender.selectedSegmentIndex),
+                                                      y: 0),
+                                              animated: true)
     }
     
     private func setupFeed() {
@@ -169,5 +191,17 @@ extension HomeViewController: UIPageViewControllerDataSource {
         return forYouPosts
         
     }
+}
+
+extension HomeViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.x == 0 || scrollView.contentOffset.x <= (view.width / 2){
+            control.selectedSegmentIndex = 0
+        }
+        else if scrollView.contentOffset.x > (view.width / 2) {
+            control.selectedSegmentIndex = 1
+        }
+    }
+    
 }
 
--- MyTikTokApp/Controllers/Core/TabBarViewController.swift
+++ MyTikTokApp/Controllers/Core/TabBarViewController.swift
@@ -22,7 +22,7 @@ class TabBarViewController: UITabBarController {
         let notifications = NotificationsViewController()
         let profile = ProfileViewController()
         
-        home.title = "Home"
+        //home.title = "Home"
         explore.title = "Explore"
         notifications.title = "Notifications"
         profile.title = "Profile"
@@ -31,6 +31,10 @@ class TabBarViewController: UITabBarController {
         let nav2 = UINavigationController(rootViewController: explore)
         let nav3 = UINavigationController(rootViewController: notifications)
         let nav4 = UINavigationController(rootViewController: profile)
+        
+        nav1.navigationBar.backgroundColor = .clear
+        nav1.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        nav1.navigationBar.shadowImage = UIImage()
         
         nav1.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), tag: 1)
         nav2.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "safari"), tag: 2)
